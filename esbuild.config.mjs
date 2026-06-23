5ea9fa30a177f76edfa280a203ad46eddb8dd6fa
CLINCH <epicylon@gmail.com>
2026-06-23 22:58:43 +0200

    Update esbuild.config.mjs

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,6 +1,6 @@
 import esbuild from "esbuild";
 import process from "process";
-import builtins from "builtin-modules";
+import { builtinModules } from "node:module";
 
 const prod = (process.argv[2] === 'production');
 
